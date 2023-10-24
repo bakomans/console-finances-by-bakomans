6d583effde40e620eb9ec718192932e4be8dba55
bakomans <bakomans@gmail.com>
2023-10-24 11:28:17 +0100

    updated for loop

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+// Sudo code
+
+// Create a new GitHub repo called Console-Finances. Then, clone it to your computer.
+
+// Copy the starter files into your local git repository.
+
+// You have been given a dataset composed of arrays with two fields: Date and Profit/Losses.
+
+// Your task is to write JavaScript code that analyzes the records to calculate each of the following:
+
+// The total number of months included in the dataset.
+
+// The net total amount of Profit/Losses over the entire period.
+
+// The average of the changes in Profit/Losses over the entire period.
+
+// You will need to track what the total change in profits is from month to month and then find the average.
+
+// (Total/(Number of months - 1))
+
+// The greatest increase in profits (date and amount) over the entire period.
+
+// The greatest decrease in losses (date and amount) over the entire period.
+
+// When you open your code in the browser your resulting analysis should look similar to the following:
+
+// Financial Analysis
+// ----------------------------
+// Total Months: 86
+// Total: $38382578
+// Average Change: -2315.12
+// Greatest Increase in Profits/Losses: Feb-2012 ($1926159)
+// Greatest Decrease in Profits/Losses: Sep-2013 ($-2196167)
+// Your final code should print the analysis to the console.
+
 var finances = [
     ['Jan-2010', 867884],
     ['Feb-2010', 984655],
@@ -119,9 +154,9 @@ var finances = [
   }
   
   // Calculate the average 
-  var averageChange = (changeInProfits / (totalMonths - 1));
+  var averageChange = (changeInProfits / (totalMonths - 1)).toFixed(2); //<-- taken from w3school
   
-  // Print the financial analysis to the console
+  // Console log the data ;
   console.log("Financial Analysis by bakomans");
   console.log("----------------------------");
   console.log("Total Months: " + totalMonths);
@@ -129,4 +164,5 @@ var finances = [
   console.log("Average Change: " + averageChange);
   console.log("Greatest Increase in Profits/Losses: " + greatestIncrease.date + " (" + greatestIncrease.amount + ")");
   console.log("Greatest Decrease in Profits/Losses: " + greatestDecrease.date + " (" + greatestDecrease.amount + ")");
+
   
